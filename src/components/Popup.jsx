1d891d17e3fd0eafe0dcb926a6a9e0cf76066946
jdalmeria-tech <alm24008@byui.edu>
2025-04-17 15:12:00 +0800

    added a popup component (:

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -2,14 +2,14 @@ function Popup () {
 
   return(
     <>
-    <div className="popup">
-      <span>Are you sure?</span>
-      <div className="popup__btns">
-        <button className="popup__btn">Confirm</button>
-        <button className="popup__btn popup__btn--cancel">Cancel</button>
+      <div className="popup">
+        <span>Are you sure?</span>
+        <div className="popup__btns">
+          <button className="popup__btn">Confirm</button>
+          <button className="popup__btn popup__btn--cancel">Cancel</button>
+        </div>
       </div>
-    </div>
-    <div className="backdrop"></div>
+      <div className="backdrop"></div>
     </>
   )
 }
