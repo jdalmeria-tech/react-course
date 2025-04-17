8eff55901f4d77d3c4ed9916c92eb6c9e4330201
jdalmeria-tech <alm24008@byui.edu>
2025-04-17 15:03:44 +0800

    learned basics of components (:

---
+++ src/components/Todo.jsx
@@ -0,0 +1,13 @@
+function Todo() {
+
+  return (
+    <>
+     <div className="todo-item">
+      <h2>To do Item</h2>
+      <button>Delete</button>
+     </div>
+    </>
+  )
+}
+
+export default Todo
---
+++ src/components/TodoTitle.jsx
@@ -0,0 +1,10 @@
+function TodoTitle() {
+  
+  return(
+   <>
+      <h1>My To Do List</h1>
+   </>
+  )
+}
+
+export default TodoTitle
