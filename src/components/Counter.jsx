ec1b431e9a95b01e8da857ee206a034c1f72a490
jdalmeria-tech <alm24008@byui.edu>
2025-04-21 23:34:49 +0800

    using callback functions since new state depends on the previous state (:

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -10,11 +10,20 @@ function Counter () {
           Counter: {count}
         </h1>
         <button
-        onClick={() => setCount(count +1 )}
+         onClick={() => {
+          setCount((prevCount) => prevCount + 1);
+          setCount((prevCount) => prevCount + 1);
+         }}
         >
           Increment
         </button>
-        <button onClick={() => setCount(count - 1)}>Decrement</button>
+        <button onClick={() => {
+          setCount((prevCount) => prevCount - 1);
+          setCount((prevCount) => prevCount - 1);
+         }}
+        >
+          Decrement
+        </button>
         <button onClick={() => setCount(0)}>Reset</button>
       </div>
     </>
@@ -22,3 +31,6 @@ function Counter () {
 }
 
 export default Counter
+
+// using callback functions since new state depends on the previous state (:
+// see notes on notion: https://www.notion.so/12076341f38c80d4a152ded1b7f40060?v=12076341f38c81d5833a000cdbda2594&p=1dc76341f38c801ebf40c1bd9a745c53&pm=s
